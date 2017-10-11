67a91dbbb86c47cd091e225a98b139546803177c
Paulo <32635078+pas21dark@users.noreply.github.com>
2017-10-11 11:22:31 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+	var mySrc = myImage.getAttribute('src');
+	if (mySrc === 'images/firefox.jpg') {
+		myImage.setAttribute ('src', 'images/chrome.jpg');
+}else{
+	myImage.setAttribute ('src', 'images/firefox.jpg');
+}
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+		var myName = prompt('Please enter your name.');
+		localStorage.setItem('name', myName);
+		myHeading.textContent = 'Hi there!,' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+		setUserName();
+}else{
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Hi there,' + storedName;
+
+}
+myButton.onclick = function() {
+	setUserName();
+}
+
